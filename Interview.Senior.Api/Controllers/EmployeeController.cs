78b00d24985537eb89cb026e4fd5809d27abe79c
Gabriel Suyama <gabriel.suyama@capgemini.com>
2021-04-07 22:52:15 -0300

    Implementing improvements

---
+++ Interview.Senior.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using Interview.Senior.Domain;
+using Interview.Senior.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Interview.Senior.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public IEmployeeCalculator EmployeeCalculator { get; set; }
+
+        public EmployeeController(IEmployeeCalculator employeeCalculator)
+        {
+            EmployeeCalculator = employeeCalculator;
+        }
+
+        [HttpPut]
+        public void AddEmployee(Employee employee) 
+        {
+            EmployeeCalculator.AddEmployee(employee);
+        }
+
+        [HttpPost]
+        public void IncreaseSalary(Employee employee, double percentageToIncrease) 
+        {
+            EmployeeCalculator.IncreaseSalary(employee, percentageToIncrease);
+        } 
+    }
+}
---
+++ Interview.Senior.Api/Controllers/FinantialCalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using Interview.Senior.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Interview.Senior.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FinantialCalculatorController : ControllerBase
+    {
+        private IFinantialCalculator _finantialCalculator;
+
+        public FinantialCalculatorController(IFinantialCalculator finantialCalculator) 
+        {
+            _finantialCalculator = finantialCalculator;
+        }
+
+        [HttpGet]
+        public double CalculateFees([FromBody] int num1, [FromBody] double feesPercentage) 
+        {
+            return _finantialCalculator.CalculateFees(num1, feesPercentage);
+        }
+    }
+}
